85093bdf702e3b70eb6f42b23f2e7720d2a6bb80
Gennadiy Shpak <gena_shpak@ukr.net>
2024-01-11 16:15:49 +0200

    Basic Structure

---
+++ bot_helper/address_book.py
@@ -0,0 +1,275 @@
+from collections import UserDict
+from datetime import date
+from re import match
+import pickle
+import json
+
+
+class WrongBirthday(Exception):
+    pass
+class ExistsPhone(Exception):
+    pass
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    def __repr__(self):
+        return str(self.value)
+
+
+class Phone(Field):
+    def __init__(self, value):
+        self.__value = ""
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__value
+        
+    @value.setter
+    def value(self, new_value):
+        if self.is_valid_phone(new_value):
+            norm_phone = self.normalis_phone(new_value)
+            self.__value =  norm_phone
+        else:
+            raise ValueError
+ 
+    def is_valid_phone(self, value):
+        if  value!= "":
+            # if match(r"^[\+]?3?8?[\s]?\(?0\d{2}?\)?[\s]?\d{3}[\s|-]?\d{2}[\s|-]?\d{2}$", value) != None:
+            if match(r"^[\+]?3?8?\(?\d{3}?\)?\d{3}[\-]?\d{2}[\-]?\d{2}$", value) != None:
+                return True
+            else:
+                return False
+
+    def normalis_phone(self, value):
+        norm_mob = value.replace("+","")\
+                        .replace(" ","")\
+                        .replace("(","")\
+                        .replace(")","")\
+                        .replace("-","")
+        if len(norm_mob) == 10:
+            return "+38" + norm_mob
+        if len(norm_mob) == 11:
+            return "+3" + norm_mob
+        if len(norm_mob) == 12:
+            return "+" + norm_mob
+        return ""
+
+    def __eq__(self, other):
+        if isinstance(other, Phone):
+            return self.value == other.value
+        return NotImplemented
+    
+
+class Birthday(Field):
+    def __init__(self, value):
+        self.__value = ""
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__value
+    
+    @value.setter
+    def value(self, new_value):
+        if self.is_valid_birthday(new_value):
+            norm_birthday = self.normalis_birthday(new_value)
+            if norm_birthday != "":
+                self.__value = norm_birthday
+            else:
+                raise WrongBirthday
+        else:
+            raise WrongBirthday
+
+    def is_valid_birthday(self, value):
+        if value != "":
+            if match(r"^\d{2}['\s'|\-|'.'|:]{1}\d{2}[\s|\-|'.'|:]{1}\d{4}$|^\d{4}['\s'|\-|'.'|:]{1}\d{2}[\s|\-|'.'|:]{1}\d{2}$", value) != None:
+                return True
+            else:
+                return False
+            
+    def normalis_birthday(self, new_value):
+        norm_birthday = new_value.replace(".",",")\
+                    .replace(" ",",")\
+                    .replace("-",",")\
+                    .replace(":",",")
+        date_birthday = norm_birthday.split(",")
+        if len(date_birthday[0]) == 4:
+            try:
+                return date(int(date_birthday[0]), int(date_birthday[1]), int(date_birthday[2]))
+            except:
+                raise WrongBirthday
+        else:
+            try:
+                return date(int(date_birthday[2]), int(date_birthday[1]), int(date_birthday[0]))
+            except:
+                raise WrongBirthday
+        
+    def __sub__(self, other):
+        birthday_month = self.value.month
+        birthday_day = self.value.day
+        my_birthday = date(other.value.year, birthday_month, birthday_day)
+        date_today = other.value
+        if my_birthday < date_today:
+            my_birthday = my_birthday.replace(year=date_today.year + 1)
+        day_to_birthday = my_birthday - date_today
+        return day_to_birthday.days
+    
+    def __str__(self):
+        return self.value.strftime("%d.%m.%Y")
+    
+
+class Record:
+    def __init__(self, name, birthday = None):
+        if birthday is not None:
+            self.birthday = Birthday(birthday)
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        phone_obj = Phone(phone)
+        if phone_obj not in self.phones:
+                self.phones.append(phone_obj)
+
+    def remove_phone(self, phone):
+        search_phone = Phone(phone)
+        self.phones.remove(search_phone)
+
+    def edit_phone(self, phone, new_phone):
+        search_phone = Phone(phone)
+        chandge_phone = Phone(new_phone)
+        index = self.phones.index(search_phone)
+        self.phones[index] = chandge_phone
+    
+    def find_phone(self, phone):
+        search_phone = Phone(phone)
+        for item in self.phones:
+            if item == search_phone:
+                return item
+                
+    def days_to_birthday(self):
+        today = Birthday(date.today().strftime("%Y %m %d"))
+        return self.birthday - today
+
+    def __str__(self):
+    
+        if hasattr(self, "birthday"):
+            return f"Contact name: {self.name.value}, phones: {', '.join(p.value for p in self.phones)}, birthday: {date.strftime(self.birthday.value, '%d.%m.%Y')}"
+        else:
+            return f"Contact name: {self.name.value}, phones: {', '.join(p.value for p in self.phones)}"
+        
+
+class AddressBook(UserDict):
+    qua_for_iter = 2
+    list_for_iter = []
+
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        record = self.data.get(name)
+        if record is not None:
+            return record
+        else:
+            raise KeyError
+    
+    def delete(self, name):
+        self.data.pop(name)
+    
+    def finde_records(self, search = None):
+        list_rec = []
+        for name, records in self.data.items():
+            if search.lower() in name.lower():
+                list_rec.append(records)
+            else:
+                for phones in records.phones:
+                    if search in phones.value:
+                        list_rec.append(records)
+                        break
+        return list_rec
+    
+    def exists_phone(self, phone = None):
+        if phone is not None:
+            phone_ = Phone(phone)
+            for record_ in self.data.values():
+                if phone_ in record_.phones:
+                    raise ExistsPhone
+
+    def __next__(self):
+        if len(self.list_for_iter) == len(self.data):
+            self.list_for_iter.clear()
+            raise StopIteration
+        iter = 0
+        for_return = []
+        for key, value in self.data.items():
+            if key in self.list_for_iter:
+                pass
+            else:
+                for_return.append(str(value))
+                self.list_for_iter.append(key)
+                iter += 1
+            if len(for_return) == self.qua_for_iter:
+                break
+        # return for_return
+        return f"{'; '.join(i for i in for_return)} \n"
+
+    def __iter__(self):
+        return self
+    
+    def save_to_file_pickle(self, file_name):
+        with open(file_name, 'wb') as file:
+            pickle.dump(self, file)
+    
+    def load_from_file_pickle(self, file_name):
+        with open(file_name, 'rb') as file:
+            return pickle.load(file)
+        
+    def save_to_file_json(self, file_name):
+        data = {}
+        dict_phones = {}
+        list_phones = []
+        for name_i, records_i in self.data.items():
+            for i_phone in records_i.phones:
+                list_phones.append(i_phone.value)
+            dict_phones["phone"] = list_phones
+            if hasattr(records_i, "birthday"):
+                data[name_i] = [dict_phones, {"birthday": records_i.birthday.value.strftime("%Y %m %d")}]
+            else:
+                data[name_i] = [dict_phones]
+            list_phones = []
+            dict_phones = {}
+        with open(file_name, 'w') as file:
+            json.dump(data, file)
+
+    def load_from_file_json(self, file_name):
+        data = None
+        with open(file_name, 'r') as file:
+            data = json.load(file)
+        ret_book = self
+        for j_name, j_records in data.items():
+            if len(j_records) == 2:
+                record = Record(j_name, j_records[1].get("birthday"))
+            else:
+                record = Record(j_name)
+            for j_phone in j_records[0].get("phone"):
+                record.add_phone(j_phone)
+            ret_book.add_record(record)
+        return ret_book
+    
+    def __str__(self):
+        ret_list = ""
+        for record in self:
+            ret_list += str(record)
+        return ret_list
+
+            
+if __name__ == "__main__":
+    pass
---
+++ bot_helper/main.py
@@ -0,0 +1,164 @@
+# from pathlib import Path
+# import bot_helper.address_book as book
+import address_book as book
+
+def input_error(func):
+    def inner(my_book, val):
+        try:
+            return_data = func(my_book, val)
+        except IndexError:
+            return_data = "Give me name and phone please"
+        except TypeError:
+            return_data = "Wrong command, try again"
+        except KeyError:
+            return_data = "Wrong user, repeat please"
+        except ValueError:
+            return_data = "Wrong number, repeat please"
+        except book.WrongBirthday:
+            return_data = "Wrong birthday, repeat please"
+        except book.ExistsPhone:
+            return_data = "Phone is exist"
+        return return_data
+    return inner
+
+
+def handler_hello(my_book, _ = None):
+    return "How can I help you?"
+
+def handler_add(my_book, list_):
+    my_book.exists_phone(list_[1])
+    try:
+        record = my_book.find(list_[0].capitalize())
+    except:
+        if len(list_) == 3:
+            record = book.Record(list_[0].capitalize(),list_[2])
+        else:
+            record = book.Record(list_[0].capitalize())
+        record.add_phone(list_[1])
+        my_book.add_record(record)
+    else:
+        record.add_phone(list_[1])
+        my_book.add_record(record)
+    return "Command successfully complete"
+
+def handler_change(my_book, list_):
+    my_book.exists_phone(list_[2])
+    record = my_book.find(list_[0].capitalize())
+    if record is not None:
+        record.edit_phone(list_[1], list_[2])
+    return f"Phone {list_[1]} from user {list_[0].capitalize()} successfully chandget to phone {list_[2]}"
+    
+def handler_show_all(my_book, _ = None):
+    return my_book
+
+def handler_exit(my_book, _ = None):
+    return "Good bye!"
+
+def handler_find(my_book, list_):
+    list_rec = my_book.finde_records(list_[0].capitalize())
+    if len(list_rec) != 0:
+        ret_book = book.AddressBook()
+        for rec_ in list_rec:
+            ret_book.add_record(rec_)
+        return ret_book
+    else:
+        return "Contact not found"
+    
+def handler_delete_phone(my_book, list_):
+    record = my_book.find(list_[0].capitalize())
+    record.remove_phone(list_[1])
+    return f"Phone {list_[1]} of user {list_[0].capitalize()} successfully deleted"
+
+def handler_delete_user(my_book, list_):
+    print(list_[0].capitalize())
+    my_book.delete(list_[0].capitalize())
+    return f"User {list_[0].capitalize()} successfully deleted"
+
+def handler_next_birthday(my_book, list_):
+    record = my_book.find(list_[0].capitalize())
+    days = record.days_to_birthday()
+    return f"Next birthday for user {list_[0].capitalize()} after {days} days"
+
+
+def handler_help(my_book = None, _ = None):
+    help_string = '''
+                Hellow, you can us next command with format:\n
+                help - for help\n
+                hello - for hello\n
+                add <user_name> <phone(10 or 13 number)> [birthday] - for add user, if user is exist will be added phone to user\n
+                change <user_name> <phone_from_chandge> <phone_to_chandge> - for chandge phone\n
+                show all - for show all records\n
+                good bye | close | exit - for exit\n
+                find <some_letters> | <some_nombers> - for find record by name or phone\n
+                delete phone <user_name> <phone> - for delete phone from user\n
+                delete user <user_name> - for delete user from address book
+
+                variation format for telefon number:
+                +38(055)111-22-33
+                38(055)111-22-34
+                8(055)111-22-35
+                (055)111-22-36
+                055111-22-37
+                and all variant without "-"
+                '''
+    return help_string
+
+NAME_COMMANDS = {
+
+    "help": handler_help,
+    "hello": handler_hello,
+    "add": handler_add,
+    "change": handler_change,
+    "showall": handler_show_all,
+    "goodbye": handler_exit,
+    "close": handler_exit,
+    "exit": handler_exit,
+    "find": handler_find,
+    "deletephone": handler_delete_phone,
+    "deleteuser": handler_delete_user,
+    "nextbirthday": handler_next_birthday
+}
+
+
+def defs_commands(comm):
+    return NAME_COMMANDS[comm]
+
+
+@input_error
+def parser_command(my_book, command):
+    list_command = command.split(" ")
+    if list_command[0] in NAME_COMMANDS:
+        any_command = defs_commands(list_command[0])
+        ret_rezault = any_command(my_book, list_command[1:])
+        return ret_rezault
+    elif len(list_command) > 1 and list_command[0]+list_command[1] in NAME_COMMANDS:
+        any_command = defs_commands(list_command[0]+list_command[1])
+        ret_rezault = any_command(my_book, list_command[2:])
+        return ret_rezault
+    else:
+        any_command = defs_commands()
+        return ret_rezault
+
+
+def main():
+    print(handler_help())
+    # file_name_p = "bot_helper\\book_pickle.bin"
+    file_name_j = "bot_helper\\book_json.json"
+    # file_name_j = Path("E:\pyton_proj\Go-IT\\bot_helper\\bot_helper\\book_json.json")
+    # my_book_p = book.AddressBook()
+    my_book_j = book.AddressBook()
+    # my_book = my_book_p.load_from_file_pickle(file_name_p) 
+    my_book = my_book_j.load_from_file_json(file_name_j)
+    while True:
+        command = input("please enter command ").lower()
+        ret_rezault = parser_command(my_book, command)
+        if ret_rezault:
+            print(ret_rezault)
+            if ret_rezault == "Good bye!":
+                # my_book.save_to_file_pickle(file_name_p)
+                my_book.save_to_file_json(file_name_j)
+                exit()
+
+        
+if __name__ == "__main__":
+    main()
