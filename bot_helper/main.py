64098545b4a6d4ccd6307b7ff2cd95424b0aa769
ElenaPolkhovaS <148061037+ElenaPolkhovaS@users.noreply.github.com>
2024-01-17 16:44:22 +0200

    pep_main

--- bot_helper/main.py
+++ bot_helper/main.py
@@ -1,9 +1,10 @@
+"""Консольний бот для управління додатком"""
+import os
 import bot_helper.address_book as book
 import bot_helper.note_book as notebook
 import bot_helper.pretty as pretty
 from bot_helper.clean import sorting_files
 from bot_helper.commands import *
-import os
 
 # from . import address_book as book
 # from . import note_book as notebook
@@ -11,16 +12,14 @@ import os
 # from .clean import sorting_files
 # from .commands import *
 
-
 # import address_book as book
 # import note_book as notebook
 # import pretty
 # from clean import sorting_files
 # from commands import *
 
-
-
 def input_error(func):
+    """Функія-декоратор, що ловить помилки вводу"""
     def inner(my_book, val):
         try:
             return_data = func(my_book, val)
@@ -53,9 +52,13 @@ def input_error(func):
 
 
 def handler_hello(my_book, _ = None):
+    """Метод обробляє команду 'hello'
+    """
     return "How can I help you?"
 
 def handler_add(my_book, list_):
+    """Метод обробляє команду 'add'
+    """
     if list_[0] == "":
         raise IndexError
     my_book.exists_phone(list_[1])
@@ -84,13 +87,17 @@ def handler_add(my_book, list_):
     return "Command successfully complete"
 
 def handler_change(my_book, list_):
+    """Метод обробляє команду 'change'
+    """
     my_book.exists_phone(list_[2])
     record = my_book.find(list_[0].capitalize())
     if record is not None:
         record.edit_phone(list_[1], list_[2])
-    return f"Phone {list_[1]} from user {list_[0].capitalize()} successfully chandget to phone {list_[2]}"
+    return f"Phone {list_[1]} from user {list_[0].capitalize()} successfully changet to phone {list_[2]}"
 
 def handler_add_email(my_book, list_):
+    """Метод обробляє команду 'email-add'
+    """
     record = my_book.find(list_[0].capitalize())
     if record is not None:
         new_email = ' '.join(list_[1:])
@@ -98,12 +105,16 @@ def handler_add_email(my_book, list_):
         return f"To user {list_[0].capitalize()} successfully added e-mail:\n\t {new_email}"
 
 def handler_delete_email(my_book, list_):
+    """Метод обробляє команду 'email-delete'
+    """
     record = my_book.find(list_[0].capitalize())
     if record is not None:
         record.delete_email()
         return f"From user {list_[0].capitalize()} successfully deleted e-mail."
 
 def handler_replace_email(my_book, list_):
+    """Метод обробляє команду 'email-replace'
+    """
     record = my_book.find(list_[0].capitalize())
     if record is not None:
         email = record.emails.value
@@ -116,6 +127,8 @@ def handler_replace_email(my_book, list_):
             record.add_email(email)
 
 def handler_add_memo(my_book, list_):
+    """Метод обробляє команду 'memo-add'
+    """
     record = my_book.find(list_[0].capitalize())
     if record is not None:
         new_memo = ' '.join(list_[1:])
@@ -123,12 +136,16 @@ def handler_add_memo(my_book, list_):
         return f"To user {list_[0].capitalize()} successfully added memo:\n\t {new_memo}"
 
 def handler_delete_memo(my_book, list_):
+    """Метод обробляє команду 'memo-delete'
+    """
     record = my_book.find(list_[0].capitalize())
     if record is not None:
         record.delete_memo()
         return f"From user {list_[0].capitalize()} successfully deleted memo."
 
 def handler_replace_memo(my_book, list_):
+    """Метод обробляє команду 'memo-replace'
+    """
     record = my_book.find(list_[0].capitalize())
     if record is not None:
         if record.memos is not None:
@@ -144,6 +161,8 @@ def handler_replace_memo(my_book, list_):
             return handler_add_memo(my_book, list_)
 
 def handler_add_addr(my_book, list_):
+    """Метод обробляє команду 'address-add'
+    """
     record = my_book.find(list_[0].capitalize())
     if record is not None:
         new_addr = ' '.join(list_[1:])
@@ -151,12 +170,16 @@ def handler_add_addr(my_book, list_):
         return f"To user {list_[0].capitalize()} successfully added address:\n\t {new_addr}"
 
 def handler_delete_addr(my_book, list_):
+    """Метод обробляє команду 'address-delete'
+    """
     record = my_book.find(list_[0].capitalize())
     if record is not None:
         record.delete_address()
         return f"From user {list_[0].capitalize()} successfully deleted address."
 
 def handler_replace_addr(my_book, list_):
+    """Метод обробляє команду 'address-replace'
+    """
     record = my_book.find(list_[0].capitalize())
     if record is not None:
         if record.address is not None:
@@ -172,16 +195,19 @@ def handler_replace_addr(my_book, list_):
             return handler_add_addr(my_book, list_)
 
 def handler_show_all(my_book, _ = None):
+    """Метод обробляє команду 'show-all'
+    """
     if my_book:
         return my_book
-    else:
-        return 'No users'
-    # return my_book
+    return 'No users'
 
 def handler_exit(my_book, _ = None):
+    """Метод обробляє команди виходу """
     return "Good bye!"
 
 def handler_find(my_book, list_):
+    """Метод обробляє команду 'find'
+    """
     list_rec = my_book.find_records(list_[0].capitalize())
     if len(list_rec) != 0:
         ret_book = book.AddressBook()
@@ -189,10 +215,11 @@ def handler_find(my_book, list_):
         for rec_ in list_rec:
             ret_book.add_record(rec_)
         return ret_book
-    else:
-        return "Contact not found"
+    return "Contact not found"
 
 def handler_find_birthday(my_book, list_):
+    """Метод обробляє команду 'find-birthday'
+    """
     qua_days = list_[0]
     if qua_days == "":
         qua_days = 10
@@ -205,28 +232,32 @@ def handler_find_birthday(my_book, list_):
         for rec_ in list_rec:
             ret_book.add_record(rec_)
         return ret_book
-    else:
-        return f"Contact for {qua_days} days not found"
+    return f"Contact for {qua_days} days not found"
 
-    
 def handler_delete_phone(my_book, list_):
+    """Метод обробляє команду 'delete-telephone'
+    """
     record = my_book.find(list_[0].capitalize())
     record.remove_phone(list_[1])
     return f"Phone {list_[1]} of user {list_[0].capitalize()} successfully deleted"
 
 def handler_delete_user(my_book, list_):
+    """Метод обробляє команду 'delete-user'
+    """
     my_book.delete(list_[0].capitalize())
     return f"User {list_[0].capitalize()} successfully deleted"
 
 def handler_next_birthday(my_book, list_):
+    """Метод обробляє команду 'next-birthday'
+    """
     record = my_book.find(list_[0].capitalize())
     days = record.days_to_birthday()
     return f"Next birthday for user {list_[0].capitalize()} after {days} days"
 
 #Coded by Illia
 
-#Додавання нотатки
 def handler_add_note(my_book, list_):
+    """Метод додає нотаток"""
     try:
         record = my_book.find(list_[0].capitalize())
     except:
@@ -236,75 +267,72 @@ def handler_add_note(my_book, list_):
     my_book.add_record(record)
     return "Command successfully complete"
 
-#Змінення тексту нотаток
 def handler_change_note(my_book, list_):
+    """Метод змінює текст нотаток"""
     record = my_book.find(list_[0].capitalize())
     if record is not None:
         record.edit_text(list_[1])
     return f"Text from note {list_[0].capitalize()} successfully changed"
 
-#Пошук нотаток
 def handler_find_note(my_book, list_):
+    """Метод пошуку нотаток"""
     list_rec = my_book.find_records(list_[0].capitalize())
     if len(list_rec) != 0:
         ret_book = notebook.NoteBook()
         for rec_ in list_rec:
             ret_book.add_record(rec_)
         return ret_book
-    else:
-        return "Note not found"
-    
-#Пошук нотаток за тегом
+    return "Note not found"
+
 def handler_find_note_by_tag(my_book, list_):
+    """Метод шукає нотаток за тегом"""
     list_rec = my_book.find_records_by_tag(list_[0].lower())
     if len(list_rec) != 0:
         ret_book = notebook.NoteBook()
         for rec_ in list_rec:
             ret_book.add_record(rec_)
         return ret_book
-    else:
-        return "Note not found"
+    return "Note not found"
 
-#Видалення тегу
 def handler_delete_tag(my_book, list_):
+    """Метод видаляє тег"""
     record = my_book.find(list_[0].capitalize())
     record.remove_tag(list_[1].lower())
     return f"Tag {list_[1]} of note {list_[0].capitalize()} successfully deleted"
 
-#Додавання тегу
 def handler_add_tag(my_book, list_):
+    """Метод додає тег"""
     record = my_book.find(list_[0].capitalize())
     record.add_tag(list_[1])
     return f"Tag {list_[1]} of note {list_[0].capitalize()} successfully added"
 
-#Видалення нотатки
 def handler_delete_note(my_book, list_):
+    """Метод видаляє нотатки"""
     my_book.delete(list_[0].capitalize())
     return f"Note {list_[0].capitalize()} successfully deleted"
 
-#Показати всі нотатки
 def handler_show_all_notes(my_book, _=None):
+    """Метод показує всі нотатки"""
     if my_book:
         return my_book
-    else:
-        return 'No notes'
-    # return my_book
+    return 'No notes'
 
-#Вибір режиму (телефонна книга або нотатки)
 def mode_change(my_book = None, _ = None):
+    """Метод вибирає режим (книга контактів чи нотатки)"""
     i = True
     while i:
         mode = input("Please choose mode\n 1. Address book\n 2. Notes\n 3. Sort folder\n")
         if mode == "1" or mode == "2" or mode == "3":
             return mode
-        else:
-            print("Wrong number!")
+        print("Wrong number!")
 
 def handler_help(my_book = None, _ = None):
+    """Метод обробляє команду 'help'
+    """
     help_list = [
         ['help', 'command description'],
         ['hello', 'greets the user'],
-        ['add <name> \[phone] \[birthday] \[Email] \[postal address] \[memos]',
+        ['add <name> / [phone] / [birthday] / [Email] / [postal address] / [memos]',
         'for add user, if user is exist will be added\n'
         'variation format for telefon number:\n'
         '+38(055)111-22-33\n'
@@ -315,14 +343,14 @@ def handler_help(my_book = None, _ = None):
         'and all variant without "-"'],
         ['change <name> <from phone> <to phone>', 'for chandge phone'],
         ['show-all' , 'for show all records'],
-        ['find <some letters> | find <some nombers>', 'for find record by name or phone'],
-        ['delete-phone <user> <phone>', 'for delete phone from user'],
+        ['find <some letters> | find <some numbers>', 'for find record by name or phone'],
+        ['delete-telephone <user> <phone>', 'for delete phone from user'],
         ['delete-user <user>', 'for delete user from address book'],
         ['email-add <name> <email text>', 'to add e-mail to user'],
         ['email-delete <name>', 'to delete Email from user'],
         ['email-replace <name> <new Email>', 'to replace existing Email with new text'],
         ['next-birthday <name>', 'shows the number of days until the subscriber`s next birthday'],
-        ['finde-birthday \[number of days]', 'displaying a list of subscribers for the nearest specified number of days'],
+        ['finde-birthday / [number of days]', 'displaying a list of subscribers for the nearest specified number of days'],
         ['memo-add <name> <note text>',
             'to add note to user (max.240 printable characters)'],
         ['memo-delete <name>', 'to delete note from user'],
@@ -330,14 +358,16 @@ def handler_help(my_book = None, _ = None):
         ['address-add <name> <address text>', 'to add address to user (max.100 printable characters)'],
         ['address-delete <name>', 'to delete address from user'],
         ['address-replace <name> <new address>', 'to replace existing address at user with new text'],
-        ['add-note <title> <text> \[tag]',' to add note'],
-        ['change-note <title> <new_text>',' to change text in note by title'],
-        ['show-all-notes',' to show all notes'],
-        ['find-note <some text>',' to find notes by <some_text> in title of note'],
-        ['find-note-by-tag <some text>',' to find notes by <some_text> in tags of note'],
-        ['delete-note-tag <title> <tag>',' to delete tag <tag> in note <title>'],
-        ['add-note-tag <title> <tag>',' to add tag <tag> in note <title>'],
-        ['delete-note <title>',' to delete note by <title>'],
+        ['add-note <title> <text> / [tag]', 'to add note'],
+        ['change-note <title> <new_text>', 'to change text in note by title'],
+        ['show-all-notes', 'to show all notes'],
+        ['find-note <some text>', 'to find notes by <some_text> in title of note'],
+        ['find-note-by-tag <some text>', 'to find notes by <some_text> in tags of note'],
+        ['delete-note-tag <title> <tag>', 'to delete tag <tag> in note <title>'],
+        ['add-note-tag <title> <tag>', 'to add tag <tag> in note <title>'],
+        ['delete-note <title>', 'to delete note by <title>'],
+        ['back', 'to return to user input'],
+        ['sort-folder <path>', 'sorts files in a folder path'],
         ['good-bye | close | exit', 'for exit']
     ]
 
@@ -375,7 +405,7 @@ NAME_COMMANDS = {
     "email-add": handler_add_email,
     "email-delete": handler_delete_email,
     "email-replace": handler_replace_email,
-    
+
     "memo-add": handler_add_memo,
     "memo-delete": handler_delete_memo,
     "memo-replace": handler_replace_memo,
@@ -387,19 +417,20 @@ NAME_COMMANDS = {
 
 
 def defs_commands(comm):
+    """Метод додає до команди функцію"""
     return NAME_COMMANDS[comm]
 
 
 @input_error
 def parser_command(my_book, command):
+    """Парсер команд"""
     list_command = command
     if list_command[0] in NAME_COMMANDS:
         any_command = defs_commands(list_command[0])
         ret_rezault = any_command(my_book, list_command[1:])
         return ret_rezault
-    else:
-        any_command = defs_commands()
-        return ret_rezault
+    any_command = defs_commands()
+    return ret_rezault
 
 
 current_path = os.path.abspath(os.getcwd())
@@ -407,6 +438,7 @@ file_name_phones_p = os.path.join(current_path, 'bot_helper', 'book_pickle.bin')
 file_name_notes_p = os.path.join(current_path, 'bot_helper', 'notes_book_pickle.bin')
 
 def main():
+    """Метод відновлює книги контактів та нотатки, обирає режим роботи"""
     # handler_help()
 
     if os.path.exists(file_name_phones_p):
@@ -419,7 +451,7 @@ def main():
         my_book_notes = my_book_notes_p.load_from_file_pickle(file_name_notes_p)
     else:
         my_book_notes = notebook.NoteBook()
-    
+
     while True:
         # #Вибір режиму (телефонна книга або нотатки)
         mode = mode_change()
@@ -439,6 +471,6 @@ def main():
                 my_book_phones.save_to_file_pickle(file_name_phones_p)
                 my_book_notes.save_to_file_pickle(file_name_notes_p)
                 exit()
-        
+
 if __name__ == "__main__":
     main()
